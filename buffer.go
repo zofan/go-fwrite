56358cb91c6e10182112fcdaa972395260ee94b2
andrew <0x9171@gmail.com>
2020-11-27 13:04:05 +0300

    upd

--- buffer.go
+++ buffer.go
@@ -1,18 +1,21 @@
 package fwrite
 
 import (
+	"github.com/pkg/errors"
 	"github.com/zofan/go-bits"
+	"sort"
 	"strings"
 )
 
 type StringBuffer struct {
 	buffer []string
+	writer *Writer
 
-	bufferSize int
-	writer     *Writer
+	BufferSize int
+	BufferSort bool
 }
 
-func NewStringBuffer(filePath string, bufferSize int, options bits.Bits8) (*StringBuffer, error) {
+func NewStringBuffer(filePath string, options bits.Bits8) (*StringBuffer, error) {
 	options.Set(Append)
 
 	w, err := NewWriter(filePath, options)
@@ -20,32 +23,42 @@ func NewStringBuffer(filePath string, bufferSize int, options bits.Bits8) (*Stri
 		return nil, err
 	}
 
-	return &StringBuffer{writer: w, bufferSize: bufferSize}, nil
+	return &StringBuffer{writer: w, BufferSize: 1000}, nil
 }
 
-func (b *StringBuffer) Push(r string) {
-	if len(b.buffer) >= b.bufferSize {
-		b.Save()
+func (b *StringBuffer) Push(v ...string) {
+	b.PushSlice(v)
+}
+
+func (b *StringBuffer) PushSlice(v []string) {
+	if len(b.buffer) >= b.BufferSize {
+		b.Flush()
 	}
 
-	b.buffer = append(b.buffer, r)
+	b.buffer = append(b.buffer, v...)
 }
 
-func (b *StringBuffer) PushSlice(r []string) {
-	if len(b.buffer) >= b.bufferSize {
-		b.Save()
-	}
-
-	b.buffer = append(b.buffer, r...)
-}
-
-func (b *StringBuffer) Save() {
+func (b *StringBuffer) Flush() {
 	b.buffer = uniqueStrings(b.buffer)
+
+	if b.BufferSort {
+		sort.Strings(b.buffer)
+	}
 
 	err := b.writer.WriteString(strings.Join(b.buffer, "\n") + "\n")
 	if err == nil {
 		b.buffer = []string{}
 	}
+}
+
+func (b *StringBuffer) Close() error {
+	b.Flush()
+
+	if len(b.buffer) > 0 {
+		return errors.New(`buffer is not empty`)
+	}
+
+	return b.writer.Close()
 }
 
 func uniqueStrings(s []string) []string {
